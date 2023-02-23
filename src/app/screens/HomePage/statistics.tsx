c0052c4655b869132ef0e3ae79a6ea319ca8e158
abbos97 <yourali97@gmail.com>
2023-02-23 14:02:46 +0900

    feat: develop homepage statistics component

---
+++ src/app/screens/HomePage/statistics.tsx
@@ -0,0 +1,41 @@
+import { Container } from "@mui/material";
+import { Box, Stack } from "@mui/system";
+import React from "react";
+import Marginer from "../../components/marginer";
+
+export function Statistics() {
+  return (
+    <div className="static_frame">
+      <Container>
+        <Stack
+          flexDirection={"row"}
+          justifyContent={"space-between"}
+          alignItems={"center"}
+          style={{height: "236px"}}
+        >
+            <div className="personage_img left"></div>
+            <Stack className="static_box">
+                <Box className="static_num">12</Box>
+                <Box className="static_text">Restaurants</Box>
+            </Stack>
+            <Marginer direction="vertical" width="2" height="64" bg="#E3C08D" />
+            <Stack className="static_box">
+                <Box className="static_num">8</Box>
+                <Box className="static_text">Years Experience</Box>
+            </Stack>
+            <Marginer direction="vertical" width="2" height="64" bg="#E3C08D" />
+            <Stack className="static_box">
+                <Box className="static_num">50+</Box>
+                <Box className="static_text">Menu ovqatlar</Box>
+            </Stack>
+            <Marginer direction="vertical" width="2" height="64" bg="#E3C08D" />
+            <Stack className="static_box">
+                <Box className="static_num">200+</Box>
+                <Box className="static_text">Foydalanuvchilar</Box>
+            </Stack>
+            <div className="personage_img right"></div>
+        </Stack>
+      </Container>
+    </div>
+  );
+}
