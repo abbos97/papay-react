1ef4e1c9371db4b5ce98995a9dc915422a30774a
abbos97 <yourali97@gmail.com>
2023-02-24 21:49:56 +0900

    feat: homepage related publishing finish

--- src/app/screens/HomePage/recommendations.tsx
+++ src/app/screens/HomePage/recommendations.tsx
@@ -1,10 +1,118 @@
-import { Container } from "@mui/material";
+import { Box, Container, Stack } from "@mui/material";
 import React from "react";
 
 export function Recommendations() {
-    return (
-        <div className="top_article_frame">
-            <Container>Recommendations</Container>
-        </div>
-    )
+  return (
+    <div className="top_article_frame">
+      <Container>
+        <Stack flexDirection={"column"}>
+          <Box className="events_text">
+            <span className="category_title">Tafsiya qilingan maqolalar</span>
+          </Box>
+          <Stack
+            flexDirection={"row"}
+            justifyContent={"space-between"}
+            sx={{ width: "1193px" }}
+          >
+            <Stack flexDirection={"column"}>
+              <Box className="article_text">Ko’p ko’rilgan</Box>
+              <Stack className="article_box">
+                <img
+                  src="/restaurant/article-food-1.png"
+                  className="article_box_img"
+                  alt=""
+                />
+                <Stack flexDirection={"column"} sx={{ mt: "14px", ml: "10px" }}>
+                  <Stack flexDirection={"row"}>
+                    <img
+                      src="/restaurant/ellipse.png"
+                      className="article_cook"
+                      alt=""
+                    />
+                    <Box className="article_name">@jonka</Box>
+                  </Stack>
+                  <Box className="article_desc">
+                    Kebuli Rice with tomatoes s...
+                  </Box>
+                </Stack>
+              </Stack>
+              <Stack className="article_box">
+                <img
+                  src="/restaurant/article-food-1.png"
+                  className="article_box_img"
+                  alt=""
+                />
+                <Stack flexDirection={"column"} sx={{ mt: "14px", ml: "10px" }}>
+                  <Stack flexDirection={"row"}>
+                    <img
+                      src="/restaurant/ellipse.png"
+                      className="article_cook"
+                      alt=""
+                    />
+                    <Box className="article_name">@jonka</Box>
+                  </Stack>
+                  <Box className="article_desc">
+                    Kebuli Rice with tomatoes s...
+                  </Box>
+                </Stack>
+              </Stack>
+              <Box className="article_text">Ko’p yoqtirilgan</Box>
+              <Stack className="article_box">
+                <img
+                  src="/restaurant/article-food-1.png"
+                  className="article_box_img"
+                  alt=""
+                />
+                <Stack flexDirection={"column"} sx={{ mt: "14px", ml: "10px" }}>
+                  <Stack flexDirection={"row"}>
+                    <img
+                      src="/restaurant/ellipse.png"
+                      className="article_cook"
+                      alt=""
+                    />
+                    <Box className="article_name">@jonka</Box>
+                  </Stack>
+                  <Box className="article_desc">
+                    Kebuli Rice with tomatoes s...
+                  </Box>
+                </Stack>
+              </Stack>
+              <Stack className="article_box" sx={{ mb: "111px" }}>
+                <img
+                  src="/restaurant/article-food-1.png"
+                  className="article_box_img"
+                  alt=""
+                />
+                <Stack flexDirection={"column"} sx={{ mt: "14px", ml: "10px" }}>
+                  <Stack flexDirection={"row"}>
+                    <img
+                      src="/restaurant/ellipse.png"
+                      className="article_cook"
+                      alt=""
+                    />
+                    <Box className="article_name">@jonka</Box>
+                  </Stack>
+                  <Box className="article_desc">
+                    Kebuli Rice with tomatoes s...
+                  </Box>
+                </Stack>
+              </Stack>
+            </Stack>
+
+            <Stack flexDirection={"column"}>
+              <Box className="article_text">Mashhurlar</Box>
+              <Stack className="celebrities_box">
+                <Box className="celebrities_box_text">Mashhurlar etirofi</Box>
+                <img src="/auth/jony.png" className="celebrities_box_img"  alt="" />
+              </Stack>
+              <Stack className="celebrities_box celebrities_box2" >
+                <Box className="celebrities_box_text">Mashhurlar etirofi</Box>
+                <img src="/auth/munis.png" className="celebrities_box_img"  alt="" />
+              </Stack>
+            </Stack>
+          </Stack>
+        </Stack>
+      </Container>
+    </div>
+  );
 }
