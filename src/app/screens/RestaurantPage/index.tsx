dac0f91958e4a72bcf60a7ffd0a029ea9474d82f
abbos97 <yourali97@gmail.com>
2023-02-24 22:46:31 +0900

    feat: develop nester routing for restaurant screen

--- src/app/screens/RestaurantPage/index.tsx
+++ src/app/screens/RestaurantPage/index.tsx
@@ -1,10 +1,27 @@
 import React from "react";
-import { Container } from "@mui/material"
+import { Container } from "@mui/material";
+import { Route, Switch, useRouteMatch } from "react-router-dom";
+import { ChosenDish } from "./chosenDish";
+import { AllRestaurants } from "./allRestaurants";
+import { OneRestaurant } from "./oneRestaurant";
+
 
 export function RestaurantPage() {
-    return (
-        <Container>
-            RestaurantPage
-        </Container>
-    )
+    const restaurant = useRouteMatch();
+    console.log(restaurant)
+  return (
+    <div className="restaurant_page">
+      <Switch>
+        <Route path={`${restaurant.path}/dish/:dish_id`}>
+            <ChosenDish/>
+        </Route>
+        <Route path={`${restaurant.path}/:restaurant_id`}>
+        <OneRestaurant />
+        </Route>
+        <Route path={`${restaurant.path}`}>
+            <AllRestaurants/>
+        </Route>
+      </Switch>
+    </div>
+  );
 }
